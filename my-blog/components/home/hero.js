2b7eed03c391a5448bf2281329391d607156728d
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-10-12 18:07:03 +0000

    hero section

--- my-blog/components/home/hero.js
+++ my-blog/components/home/hero.js
@@ -1,5 +1,25 @@
+import Image from "next/image";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="w-full h-screen flex flex-col md:flex-row bg-stone-50 text-stone-900 items-center justify-center gap-8">
+      <div className="w-64 h-64 rounded-full overflow-hidden shadow-2xl">
+        <Image
+          src="/images/site/my baby.jpg"
+          alt="a rare picture of WebDevTolu"
+          width={300}
+          height={400}
+          className="w-full h-full object-cover object-top"
+        />
+      </div>
+      <div className="flex flex-col gap-4 items-center">
+        <h1 className="text-4xl font-bold">Hi, I'm WebDevTolu</h1>
+        <p className="text-lg font-medium">
+          A full stack developer building experiences to make your life easier.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
