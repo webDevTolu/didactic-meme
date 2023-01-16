fcb639e04e129d1fddce5801b59b9953c63d6ef9
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-01-16 15:25:15 +0100

    update db access

--- nextBlog/pages/api/contact.js
+++ nextBlog/pages/api/contact.js
@@ -1,18 +1,18 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { email, name, message } = req.body;
 
     if (
       !email ||
-      !email.includes('@') ||
+      !email.includes("@") ||
       !name ||
-      name.trim() === '' ||
+      name.trim() === "" ||
       !message ||
-      message.trim() === ''
+      message.trim() === ""
     ) {
-      res.status(422).json({ message: 'Invalid input.' });
+      res.status(422).json({ message: "Invalid input." });
       return;
     }
 
@@ -26,21 +26,21 @@ async function handler(req, res) {
 
     try {
       client = await MongoClient.connect(
-        'mongodb+srv://maximilian:2YkcXq43KyPk0vqp@cluster0.ntrwp.mongodb.net/my-site?retryWrites=true&w=majority'
+        "mongodb+srv://admin:La9Y8Dyektr4Dtsh@theblog.zlws9.mongodb.net/nextBlogV2?retryWrites=true&w=majority"
       );
     } catch (error) {
-      res.status(500).json({ message: 'Could not connect to database.' });
+      res.status(500).json({ message: "Could not connect to database." });
       return;
     }
 
     const db = client.db();
 
     try {
-      const result = await db.collection('messages').insertOne(newMessage);
+      const result = await db.collection("messages").insertOne(newMessage);
       newMessage.id = result.insertedId;
     } catch (error) {
       client.close();
-      res.status(500).json({ message: 'Storing message failed!' });
+      res.status(500).json({ message: "Storing message failed!" });
       return;
     }
 
@@ -48,7 +48,7 @@ async function handler(req, res) {
 
     res
       .status(201)
-      .json({ message: 'Successfully stored message!', message: newMessage });
+      .json({ message: "Successfully stored message!", message: newMessage });
   }
 }
 
