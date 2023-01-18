63b3769f738d3342cfbf64add939302f53910dfd
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-01-18 19:36:08 +0100

    updated details

--- nextBlog/components/home-page/hero.js
+++ nextBlog/components/home-page/hero.js
@@ -1,22 +1,22 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import classes from './hero.module.css';
+import classes from "./hero.module.css";
 
 function Hero() {
   return (
     <section className={classes.hero}>
       <div className={classes.image}>
         <Image
-          src='/images/site/max.png'
-          alt='An image showing Max'
+          src="/images/site/webdevtolu.jpg"
+          alt="An image showing Tolu"
           width={300}
           height={300}
         />
       </div>
-      <h1>Hi, I'm Max</h1>
+      <h1>Hi, I'm webDevTolu</h1>
       <p>
-        I blog about web development - especially frontend frameworks like
-        Angular or React.
+        I blog about web development - especially frontend frameworks like Next
+        or React.
       </p>
     </section>
   );
--- nextBlog/pages/index.js
+++ nextBlog/pages/index.js
@@ -1,18 +1,18 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import { Fragment } from "react";
+import Head from "next/head";
 
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
+import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
 function HomePage(props) {
   return (
     <Fragment>
       <Head>
-        <title>Max' Blog</title>
+        <title>Tolu's Blog</title>
         <meta
-          name='description'
-          content='I post about programming and web development.'
+          name="description"
+          content="I post about programming and web development."
         />
       </Head>
       <Hero />
